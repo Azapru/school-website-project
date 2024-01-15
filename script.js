2515e1f2df8326ea062908795a65604af2bf1d15
Azapru <62551802+Azapru@users.noreply.github.com>
2024-01-15 17:12:23 +0100

    fix quick search lolll

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var results = document.getElementById("search-frame")
 var searchBox = document.getElementById("search")
 var gamesList = []
+var mouseOverSearch = false
 
 // Fetch games list
 fetch("games/games.json")
@@ -18,7 +19,7 @@ fetch("games/games.json")
         `
     }
 })
-.catch(error => console.error('Error fetching JSON:', error));
+.catch(error => console.error("Error fetching JSON:", error));
 
 
 function search_games() {
@@ -40,10 +41,25 @@ function search_games() {
     }
 }
 
+// Checking for input over search results frame to fix problems
+results.addEventListener("mouseover", () => {
+    mouseOverSearch = true
+});
+results.addEventListener("mouseout", () => {
+    mouseOverSearch = false
+});
+results.addEventListener("touchstart", () => {
+    mouseOverSearch = true
+});
+results.addEventListener("touchend", () => {
+    mouseOverSearch = false
+});
+
+// Show/hide search box
 setInterval(() => {
     if (document.activeElement === searchBox && searchBox.value != "") {
         results.style.display = "block"
-    } else {
+    } else if (mouseOverSearch == false) {
         results.style.display = "none"
     }
 }, 100)
