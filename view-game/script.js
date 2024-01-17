b82462aaa03030057af6560c3b3f4b0a2530d3b2
Azapru <62551802+Azapru@users.noreply.github.com>
2024-01-17 19:05:57 +0100

    add categories and reviews

--- view-game/script.js
+++ view-game/script.js
@@ -9,6 +9,7 @@ if (id != null) {
         .then(data => {
             const name = data.name;
             const description = data.description;
+            const reviews = data.reviews;
 
             document.getElementById("game-title").innerText = name;
             document.getElementById("game-desc").innerText = description;
@@ -16,6 +17,17 @@ if (id != null) {
             document.getElementById("play-btn-link").href = "../games/"+id+"/";
 
             document.title = name
+
+            // Reviews
+            for (let i = 0; i < reviews.length; i++) {
+                document.getElementById("frame-reviews").innerHTML += `
+                    <div class="paragraph review">
+                        <img src="../assets/user-default.png">
+                        <h3>@${reviews[i][0]}</h3>
+                    </div>
+                    <p>${reviews[i][1]}</p>
+                `
+            }
         })
         .catch(error => {
             document.getElementById("frame").remove();
